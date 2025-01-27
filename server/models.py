0095501e10dfadbf571501f356a48fcec2a73e16
John Gaitho <jgaitho016@gmail.com>
2025-01-27 20:19:09 +0300

    class song

--- server/models.py
+++ server/models.py
@@ -7,4 +7,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    
+
+
+# to define Song model
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    artist = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(50))
+    release_date = db.Column(db.Date)
+    duration = db.Column(db.Integer)
