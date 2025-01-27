3c3a730d428b8a3ed9c973f181f3107a5dcda0aa
John Gaitho <jgaitho016@gmail.com>
2025-01-27 20:42:43 +0300

    class playlists

--- server/models.py
+++ server/models.py
@@ -17,3 +17,19 @@ class Song(db.Model):
     genre = db.Column(db.String(50))
     release_date = db.Column(db.Date)
     duration = db.Column(db.Integer)
+
+# to define Playlist model
+class Playlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+
+
+# to define PlaylistSong model (many-to-many relationship)
+class PlaylistSong(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    playlist_id = db.Column(db.Integer, db.ForeignKey('playlist.id'), nullable=False)
+    song_id = db.Column(db.Integer, db.ForeignKey('song.id'), nullable=False)
+    order = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Integer)
