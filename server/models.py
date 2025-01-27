339bac26c575f660c10ca1aacbbef1336554bb34
John Gaitho <jgaitho016@gmail.com>
2025-01-27 20:16:44 +0300

    user class

---
+++ server/models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# to define User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    
